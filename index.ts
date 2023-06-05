e24975ca61e2d78fd632631cebd71b820e900329
volaya <volayaf@gmail.com>
2023-06-05 11:19:12 +0200

    fixed script

--- index.ts
+++ index.ts
@@ -8,15 +8,29 @@ import {
 
 import { readFileSync } from "fs";
 
-const diagram: WorkflowDiagram = JSON.parse(
-    readFileSync("workflow.json", "utf8")
-) as WorkflowDiagram;
-const connection = new BQConnection(
-    "my_connection_name",
-    "myproject.workflows_temp"
-);
-const toolbox = getToolbox(Provider.BigQuery);
-const workflow = Workflow.fromDiagram("myworkflow", diagram, toolbox);
+async function main() {
+    const diagram: WorkflowDiagram = JSON.parse(
+        readFileSync("workflow.json", "utf8")
+    ) as WorkflowDiagram;
+    const connection = new BQConnection(
+        "my_connection_name",
+        "myproject.workflows_temp"
+    );
+    const toolbox = getToolbox(Provider.BigQuery);
+    const workflow = Workflow.fromDiagram("myworkflow", diagram, toolbox);
 
-const storedproc = workflow.getAsStoredProcedure(connection, {}, true, []);
-console.log(storedproc);
+    const storedproc = await workflow.getAsStoredProcedure(
+        connection,
+        {},
+        true,
+        []
+    );
+    console.log(storedproc);
+}
+
+(async () => {
+    try {
+        const text = await main();
+        console.log(text);
+    } catch (e) {}
+})();
